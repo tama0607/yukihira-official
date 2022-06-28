312ee84928a7e6d67df3be79c966b3630a8bf760
on101tamarutaichi <72354724+yukihirasrr@users.noreply.github.com>
2022-06-29 03:37:21 +0900

    メソッド名変更

--- app/javascript/profile.js
+++ app/javascript/profile.js
@@ -2,19 +2,19 @@ $(document).on('turbolinks:load', function() {
   $(function() {
       const $changePhoto = $('.p-profile__photoImage')
 
-      function fadeOutChange(){
+      function mouseEnter(){
         $changePhoto.attr('src', 'assets/profile/rito-profile2.png');
         console.log("good")
       }
 
-      function fadeInChange(){
+      function mouseLeave(){
         $changePhoto.attr('src', 'assets/profile/rito-profile1.png');
       }
 
       function hoverImage() {
         $changePhoto.on({
-            "mouseenter": fadeOutChange,
-            "mouseleave": fadeInChange,
+            "mouseenter": mouseEnter,
+            "mouseleave": mouseLeave,
       });
     }
     hoverImage();
