d9b9776926bae5c48cce7cf3e3e89a53ba083374
on101tamarutaichi <72354724+yukihirasrr@users.noreply.github.com>
2022-06-29 08:34:19 +0900

    スマホ対応

--- app/javascript/profile.js
+++ app/javascript/profile.js
@@ -4,39 +4,34 @@ $(document).on('turbolinks:load', function() {
     const $Image1 = "assets/profile/rito-profile1.png" 
     const $Image2 = "assets/profile/rito-profile2.png"
     
-    if (window.matchMedia('(max-width: 768px)').matches) {
-      //ここにスマホ・タブレットの時のjs
-      function hoverImage() {
-        $changePhoto.on({
-          "touchstart": touchStart,
-          "touchend": touchEnd,
-        });
-      }
-      hoverImage();
-    } else {
-      //ここにPCの時のjs
-      function hoverImage() {
-        $changePhoto.on({
-          "mouseenter": mouseEnter,
-          "mouseleave": mouseLeave,
-        });
-      }
-      hoverImage();
-    }
     function mouseEnter(){
       $changePhoto.attr('src', $Image2);
+      console.log("aaa");
     }
 
     function mouseLeave(){
       $changePhoto.attr('src', $Image1);
+      console.log("bbb");
     }
 
     function touchStart(){
-      $changePhoto.attr('src', 'assets/profile/rito-profile2.png');
+      $changePhoto.attr('src', $Image2);
+      console.log("ccc");
     }
 
     function touchEnd(){
-      $changePhoto.attr('src', 'assets/profile/rito-profile1.png');
+      $changePhoto.attr('src', $Image1);
+      console.log("ddd");
     }
+
+    function hoverImage() {
+      $changePhoto.on({
+        "mouseenter": mouseEnter,
+        "mouseleave": mouseLeave,
+        "touchstart": touchStart,
+        "touchend": touchEnd,
+      });
+    }
+    hoverImage();
   });
 });
