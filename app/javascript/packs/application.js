528ce19cc269875bd3c0ace55014816c84d06727
on101tamarutaichi <72354724+yukihirasrr@users.noreply.github.com>
2022-06-28 18:25:35 +0900

    profile.jsの読み込み

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require('slider')
 require('jquery')
 require('humburger')
+require('profile')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
