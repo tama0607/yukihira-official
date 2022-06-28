ded8e7648d450e14b1209c8c1b63a9d38a2e5417
on101tamarutaichi <72354724+yukihirasrr@users.noreply.github.com>
2022-06-29 07:36:58 +0900

    スマホ対応

--- app/javascript/profile.js
+++ app/javascript/profile.js
@@ -3,7 +3,26 @@ $(document).on('turbolinks:load', function() {
     const $changePhoto = $('.p-profile__photoImage')
     const $Image1 = "assets/profile/rito-profile1.png" 
     const $Image2 = "assets/profile/rito-profile2.png"
-
+    
+    if (window.matchMedia('(max-width: 768px)').matches) {
+      //ここにスマホ・タブレットの時のjs
+      function hoverImage() {
+        $changePhoto.on({
+          "touchstart": touchStart,
+          "touchend": touchEnd,
+        });
+      }
+      hoverImage();
+    } else {
+      //ここにPCの時のjs
+      function hoverImage() {
+        $changePhoto.on({
+          "mouseenter": mouseEnter,
+          "mouseleave": mouseLeave,
+        });
+      }
+      hoverImage();
+    }
     function mouseEnter(){
       $changePhoto.attr('src', $Image2);
     }
@@ -19,15 +38,5 @@ $(document).on('turbolinks:load', function() {
     function touchEnd(){
       $changePhoto.attr('src', 'assets/profile/rito-profile1.png');
     }
-
-    function hoverImage() {
-      $changePhoto.on({
-        "mouseenter": mouseEnter,
-        "mouseleave": mouseLeave,
-        "touchstart": touchStart,
-        "touchend": touchEnd,
-      });
-    }
-    hoverImage();
   });
 });
