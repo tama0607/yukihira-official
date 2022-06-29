c3f1ae9d84ab3c72e5682fc6ebf7f634a48380d2
on101tamarutaichi <72354724+yukihirasrr@users.noreply.github.com>
2022-06-30 05:54:34 +0900

    本番環境サーバー設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,14 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: ENV["FROM_GMAIL_ADDRESSE"], #送信をする側のアドレス
+    password: ENV["FROM_GMAIL_PASSWORD"], #送信をする側のパスワード
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
