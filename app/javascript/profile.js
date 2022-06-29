55647edb05b70f1e432c495da6f79b4b84b16b8a
on101tamarutaichi <72354724+yukihirasrr@users.noreply.github.com>
2022-06-30 00:02:59 +0900

    フェードアニメーションに変更

--- app/javascript/profile.js
+++ app/javascript/profile.js
@@ -1,37 +1,6 @@
 $(document).on('turbolinks:load', function() {
   $(function() {
     const $changePhoto = $('.p-profile__photoImage')
-    const $Image1 = "assets/profile/rito-profile1.png" 
-    const $Image2 = "assets/profile/rito-profile2.png"
-    
-    function mouseEnter(){
-      $changePhoto.attr('src', $Image2);
-      console.log("aaa");
-    }
-
-    function mouseLeave(){
-      $changePhoto.attr('src', $Image1);
-      console.log("bbb");
-    }
-
-    function touchStart(){
-      $changePhoto.attr('src', $Image2);
-      console.log("ccc");
-    }
-
-    function touchEnd(){
-      $changePhoto.attr('src', $Image1);
-      console.log("ddd");
-    }
-
-    function hoverImage() {
-      $changePhoto.on({
-        "mouseenter": mouseEnter,
-        "mouseleave": mouseLeave,
-        "touchstart": touchStart,
-        "touchend": touchEnd,
-      });
-    }
-    hoverImage();
+    $changePhoto.hide().fadeIn(1000);
   });
 });
