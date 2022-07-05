53a60dfc5d2d4582197073b8b598c8c8f4603ece
on101tamarutaichi <72354724+yukihirasrr@users.noreply.github.com>
2022-07-05 12:31:41 +0900

    コントローラーの追加

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,13 @@
 class ContactsController < ApplicationController
+  before_action :check_captcha ,only: [:create]
+
   def new
     @contact = Contact.new
   end
 
   def create
     @contact = Contact.new(contact_params)
-    if @contact.save
+    if verify_recaptcha(model: @contact) && @contact.save
       ContactMailer.send_mail(@contact).deliver
       redirect_to thanks_contacts_path
     else
@@ -17,8 +19,13 @@ class ContactsController < ApplicationController
   end
 
   private
-
   def contact_params
-    params.require(:contact).permit(:name, :message)
+    params.require(:contact).permit(:name, :email, :subject, :message)
+  end
+  def check_captcha
+    unless verify_recaptcha(message: "reCAPTCHAのチェックをしてください")
+      @contact = Contact.new(contact_params)
+      render :new
+    end 
   end
 end
