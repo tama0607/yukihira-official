19d1443d6cb26a8bd334cdf1d16b2c2075b9c460
on101tamarutaichi <72354724+yukihirasrr@users.noreply.github.com>
2022-07-11 08:29:35 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'menus#home'
   get 'profile', to: 'menus#profile'
   get 'live', to: 'menus#live'
+  get 'movie', to: 'menus#movie'
   resource :contacts, only: [:new, :create] do
     get "/thanks" => "contacts#thanks"
    end
